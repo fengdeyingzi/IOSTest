eec38dd18f8668e23bc93c45d3b29fbb163d2661
xlapp <xlapp@xlappdeMac.local>
2019-05-02 19:25:38 -0700

    ...

---
+++ IOSTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestIOS
+//
+//  Created by xlapp on 2019/5/2.
+//  Copyright © 2019年 xlapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor=UIColor.black;
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
