c14436ed81f87312c5aeb5d798df84bbc89b0c9d
fengfeyingzi <2541012655@qq.com>
2019-05-24 21:29:55 +0800

    get post网络测试

---
+++ 测试app/HttpTest.m
@@ -0,0 +1,95 @@
+//
+//  HttpTest.m
+//  测试app
+//
+//  Created by mac on 2019/5/24.
+//  Copyright © 2019 xlapp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation HttpTest : NSObject
+
+
+-(void)get1
+{
+    //对请求路径的说明
+    //http://120.25.226.186:32812/login?username=520it&pwd=520&type=JSON
+    //协议头+主机地址+接口名称+？+参数1&参数2&参数3
+    //协议头(http://)+主机地址(120.25.226.186:32812)+接口名称(login)+？+参数1(username=520it)&参数2(pwd=520)&参数3(type=JSON)
+    //GET请求，直接把请求参数跟在URL的后面以？隔开，多个参数之间以&符号拼接
+    
+    //1.确定请求路径
+    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/login?username=520it&pwd=520it&type=JSON"];
+    
+    //2.创建请求对象
+    //请求对象内部默认已经包含了请求头和请求方法（GET）
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    //3.获得会话对象
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    //4.根据会话对象创建一个Task(发送请求）
+    /*
+     第一个参数：请求对象
+     第二个参数：completionHandler回调（请求完成【成功|失败】的回调）
+     data：响应体信息（期望的数据）
+     response：响应头信息，主要是对服务器端的描述
+     error：错误信息，如果请求失败，则error有值
+     */
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error == nil) {
+            //6.解析服务器返回的数据
+            //说明：（此处返回的数据是JSON格式的，因此使用NSJSONSerialization进行反序列化处理）
+            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+            
+            NSLog(@"%@",dict);
+        }
+    }];
+    
+    //5.执行任务
+    [dataTask resume];
+}
+
+//发送GET请求的第一种方法
+-(void)get2
+{
+    //1.确定请求路径
+    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/login?username=520it&pwd=520it&type=JSON"];
+    
+    //2.获得会话对象
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    //3.根据会话对象创建一个Task(发送请求）
+    /*
+     第一个参数：请求路径
+     第二个参数：completionHandler回调（请求完成【成功|失败】的回调）
+     data：响应体信息（期望的数据）
+     response：响应头信息，主要是对服务器端的描述
+     error：错误信息，如果请求失败，则error有值
+     注意：
+     1）该方法内部会自动将请求路径包装成一个请求对象，该请求对象默认包含了请求头信息和请求方法（GET）
+     2）如果要发送的是POST请求，则不能使用该方法
+     */
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        //5.解析数据
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+        NSLog(@"%@",dict);
+        
+    }];
+    
+    //4.执行任务
+    [dataTask resume];
+}
+
+//发送GET请求的第二种方法
+
+//执行结果
+//![](http://images2015.cnblogs.com/blog/450136/201601/450136-20160129131707286-1497362695.png)
+
+
+
+@end
+
--- 测试app/ViewController.swift
+++ 测试app/ViewController.swift
@@ -41,5 +41,76 @@ class ViewController: UIViewController {
         
     }
     
+    /*
+    func postTest()->Void{
+        let url = NSURL(string: "https://yzjlb.net")
+        var req = NSMutableURLRequest(url: url as! URL);
+        
+        req.httpMethod = "POST";
+       req.HTTPBody = NSData(data: Data(base64Encoded: "jhg"))
+        let request = NSMutableURLRequest(url: url as! URL)
+        request.httpMethod = "POST";
+        let args:NSString = NSString("action=test")
+        //request.httpBody = args;
+        let session = URLSession.shared
+        session.dataTask(with: url, completionHandler: (data,url,handler){
+            
+        });
+        let task: URLSessionDataTask = session.dataTask(with: (data,request,error)->{
+            
+        }){
+            (data,response,error) in
+            if error == null{
+                do{
+                    if let dict = try JSONSerialization.jsonObject(with: data, options: ""){
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    
+    func post2()->Void{
+        let url : NSURL = NSURL(string: "https://yzjlb.net")!
+        
+        //创建请求对象
+        let request : NSURLRequest = NSURLRequest(url: url as URL)
+        
+        //获取会话对象
+        let session: URLSession = URLSession.shared
+        
+        //创建一个task发送请求
+        let task: URLSessionDataTask = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            if error == nil {
+                do{
+                    
+                    //对收到的数据进行json进行解析
+                    if let dict = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)as? NSDictionary{
+                        
+                        let result = dict.object(forKey: "IMEISetFlag") as! String
+                        /*
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            if result == "1" {
+                                
+                                //返回访问成功，执行成功之后的操作
+                            }
+                        })
+ */
+                        
+                    }
+                    
+                }catch{
+                    print("错误")
+                }
+            }
+            
+        }
+        //执行任务
+        task.resume()
+    }
+    
+    */
 }
 
